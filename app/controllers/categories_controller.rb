96977bbb3f7c2dda2e64694bdaaa69eb661fcce7
vinzme <vinzblue@gmail.com>
2014-11-17 07:27:51 +0800

    Pre-course

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,10 @@
+class CategoriesController < ApplicationController
+
+
+  def show
+    @category = Category.find_by id: params[:id]
+  end
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Video.create(title: 'Big Hero', small_cover_url: 'bighero', description: 'Robotics prodigy Hiro lives in the City of Fransokyo')
+#Video.create(title: 'Blacklist', small_cover_url: 'blacklist', description: 'When former govt agent & fugitive Raymond Reddington...')
+#Video.create(title: 'Gotham', small_cover_url: 'gotham', description: 'American crime TV series developed by Bruno Heller')
+#Video.create(title: 'Jane the Virgin', small_cover_url: 'jane', description: 'The daughter of a teen mother, Jane Villanueva grew up....')
+Video.create(title: 'Breaking Bad', small_cover_url: 'breakingbad', description: 'Mild-manner high school chemistry teacher Walter White...')
+Video.create(title: 'Walking Dead', small_cover_url: 'walkingdead', description: 'American post-apocalyptic horror drama TV series...')
+Video.create(title: 'Flash', small_cover_url: 'flash', description: "At 11, Barry Allen's life changed completely when his mother died in...")
+Video.create(title: 'Guardians of the Galaxy', small_cover_url: 'guardians', description: 'Brash space adventurer Peter Quill finds himself....')
