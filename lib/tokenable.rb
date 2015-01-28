751048a720c61446a0ff40b8bb7489087d65d642
vinzme <vinzblue@gmail.com>
2015-01-29 00:00:54 +0800

    Week5

---
+++ lib/tokenable.rb
@@ -0,0 +1,13 @@
+module Tokenable
+  extend ActiveSupport::Concern
+
+  included  do
+    before_create :generate_token
+    
+    def generate_token
+      self.token = SecureRandom.urlsafe_base64
+    end
+
+  end
+
+end
